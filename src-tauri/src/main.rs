66347d7ef1ceb81400286c303ef09566f5645b88
DevTesaG <feynman.stories@gmail.com>
2025-07-30 02:27:41 -0600

    opened methods from plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,10 +3,40 @@
 
 use tauri_app_lib::init;
 
+use tauri_app_lib::{
+    get_products,
+    create_product,
+    update_product,
+    delete_product,
+    get_orders,
+    create_order,
+    get_coupons,
+    create_coupon,
+    print_receipt,
+    Database,
+    init_database,
+};
+use std::sync::Mutex;
+
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .plugin(init())
+        .invoke_handler(tauri::generate_handler![
+            get_products,
+            create_product,
+            update_product,
+            delete_product,
+            get_orders,
+            create_order,
+            get_coupons,
+            create_coupon,
+            print_receipt,
+        ])
+        .setup(|app| {
+            let db = Database(Mutex::new(init_database()?));
+            app.manage(db);
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
