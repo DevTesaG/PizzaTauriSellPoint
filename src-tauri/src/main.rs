1c3bdf03aa67d429fc2c02e7f71ac97a0d9ada2c
DevTesaG <feynman.stories@gmail.com>
2025-07-30 14:31:09 -0600

    changed imports

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -18,15 +18,15 @@ fn main() {
     tauri::Builder::default()
       .manage(Database::new("pizza_pos.db"))
       .invoke_handler(tauri::generate_handler![
-         lib::get_products,
-         lib::create_product,
-         lib::update_product,
-         lib::delete_product,
-         lib::get_orders,
-         lib::create_order,
-         lib::get_coupons,
-         lib::create_coupon,
-         lib::print_receipt
+        get_products,
+        create_product,
+        update_product,
+        delete_product,
+        get_orders,
+        create_order,
+        get_coupons,
+        create_coupon,
+        print_receipt
       ])
       .run(tauri::generate_context!())
       .expect("error while running tauri app");
